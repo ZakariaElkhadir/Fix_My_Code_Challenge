ac81767f93cd13ee205f13e55ebc0ac896df126a
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-17 16:01:46 +0000

    edit file

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -25,9 +25,9 @@ class Square():
         return "{}/{}".format(self.width, self.height)
 
 
+if __name__ == "__main__":
 
-
-s = Square(width=12, height=9)
-print(s)
-print(s.area_of_my_square())
-print(s.PermiterOfMySquare())
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
